1511bce72fc8e7dd135ca45ab476e2592cb95ae9
igumus <ibrhm.gumus@gmail.com>
2022-06-17 20:04:47 +0300

    added: go-objectstore-lib dependency

--- storage.go
+++ storage.go
@@ -1,1 +1,27 @@
 package blockstorage
+
+import (
+	"context"
+
+	"github.com/igumus/go-objectstore-lib"
+)
+
+type BlockStorage interface {
+}
+
+type storage struct {
+	store objectstore.ObjectStore
+}
+
+func NewBlockStorage(ctx context.Context, opts ...BlockStorageOption) (BlockStorage, error) {
+	ret := &storage{}
+
+	cfg, cfgErr := createConfig(opts...)
+	if cfgErr != nil {
+		return ret, cfgErr
+	}
+
+	ret.store = cfg.ostore
+
+	return ret, nil
+}
---
+++ storage_options.go
@@ -0,0 +1,43 @@
+package blockstorage
+
+import (
+	"github.com/igumus/go-objectstore-lib"
+)
+
+// A BlockStorageOption sets options.
+type BlockStorageOption func(*blockstorageConfig)
+
+// Captures/Represents BlockStorage's configuration information.
+type blockstorageConfig struct {
+	ostore objectstore.ObjectStore
+}
+
+// validate - validates given `blockstorageConfig` instance
+func validate(s *blockstorageConfig) error {
+	return nil
+}
+
+// defaultBlockstorageConfig - returns instance of `blockstorageConfig` with initial values.
+func defaultBlockstorageConfig() *blockstorageConfig {
+	return &blockstorageConfig{
+		ostore: nil,
+	}
+}
+
+// createConfig - creates new `blockstorageConfig` with given options.
+// Creates default configuration and applys options to configuration.
+// Returns configuration instance and validation result.
+func createConfig(opts ...BlockStorageOption) (*blockstorageConfig, error) {
+	cfg := defaultBlockstorageConfig()
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	return cfg, validate(cfg)
+}
+
+// WithObjectStore returns a BlockStorageOption that specifies objectstore as persistence storage
+func WithObjectStore(s objectstore.ObjectStore) BlockStorageOption {
+	return func(bc *blockstorageConfig) {
+		bc.ostore = s
+	}
+}
